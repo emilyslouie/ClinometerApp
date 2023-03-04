fe8a2aacbdc754cc6e5e8a0a6d152f984431d1e4
Emily Louie <emilyslouie@gmail.com>
2023-03-04 15:05:27 -0500

    Initial Commit

---
+++ ClinometerApp/ClinometerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinometerAppApp.swift
+//  ClinometerApp
+//
+//  Created by Emily Louie on 2023-03-04.
+//
+
+import SwiftUI
+
+@main
+struct ClinometerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
