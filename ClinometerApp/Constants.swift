b5383fedff7427b9c8e7c0542cf1c82bc23bd519
Emily Louie <emilyslouie@gmail.com>
2023-03-07 02:22:52 -0500

    Last fixes

--- ClinometerApp/Constants.swift
+++ ClinometerApp/Constants.swift
@@ -8,10 +8,10 @@
 enum Constants {
     // Calculated by subtracting median eye height from median stature for men and women, then averaging the two differences together
     // Source: https://dacowits.defense.gov/LinkClick.aspx?fileticket=EbsKcm6A10U%3D&portalid=48
-    static let averageDifferenceBetweenHeightAndEyeLevel = 11.2
+    static let averageDifferenceBetweenHeightAndEyeLevel = 0.112
 
     static let footToMetreConversion = 0.3048
     static let metreToFootConversion = 3.28084
 
-    static let inchToFootConversion = 1/12
+    static let inchToFootConversion = 1.0/12.0
 }
--- ClinometerApp/Extensions.swift
+++ ClinometerApp/Extensions.swift
@@ -5,6 +5,8 @@
 //  Created by Matthew Kee on 2023-03-07.
 //
 
+import Foundation
+
 extension Double {
     func truncate(places : Int)-> Double {
         return Double(floor(pow(10.0, Double(places)) * self)/pow(10.0, Double(places)))
